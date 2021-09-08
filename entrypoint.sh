9d1bad0f1e4aeb52b62fda27d89a27a3018b3fd6
pawan-59 <pawan@devtron.ai>
2021-09-08 13:18:23 +0530

    added to git-test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,24 @@
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
 
+sed -i "s/LTAG=.*/LTAG=\"main\";/" manifests/installation-script
+
+sed -i "s/REPO_RAW_URL=.*/REPO_RAW_URL= https:\/\/gitee.com\/devtron-labs\/devtron\/raw/" manifests/installation-script
+
+sed -i "s/url:.*/url: https:\/\/gitee.com\/devtron-labs\/devtron\/raw\/main\/manifests\/installation-script/" manifests/install/devtron-installer.yaml
+
+sed -i "29 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/devtron.git/" manifests/yamls/migrator.yaml
+
+sed -i "65 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/devtron.git/" manifests/yamls/migrator.yaml
+
+sed -i "103 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/git-sensor.git/" manifests/yamls/migrator.yaml
+
+sed -i "141 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/lens.git/" manifests/yamls/migrator.yaml
+
+git config --global user.email "pawan@devtron.ai"
+git config --global user.name "pawan_06d2"
+
+git commit -am "updated github link to gitee"
+
 case "${GITHUB_EVENT_NAME}" in
     push)
         git push -f --all target
